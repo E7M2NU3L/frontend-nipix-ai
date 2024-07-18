2b6b7595f62a5372bca506db9954ca4dd62e3fa8
E7M2NU3L <aemmanuel685210@gmail.com>
2024-07-18 20:06:59 +0530

    made a simple web app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [input, setInput] = useState('');
+  const [output, setoutput] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const objectToSend = {
+        'sentence': input
+      };
+      console.log(input);
+      const endpoint = "http://localhost:8000/ai_sentiment/home"
+
+      const response = await axios.post(endpoint, objectToSend);
+      console.log(response);
+      setoutput(response.data.output);
+    } catch (error) {
+      
+    }
+  }
+  const handleClear = () => {
+    setInput('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>
+          Nipix Ai
+        </h1>
       </header>
-    </div>
+
+      <br />
+      <main>
+        <form onSubmit={handleSubmit}>
+          <input placeholder="type somethig here.." className="px-4 py-2 rounded-md" value={input} onChange={(e) => setInput(e.target.value)} />
+          <button type="submit">
+            submit
+          </button>
+          <button onClick={handleClear}>
+            clear
+          </button>
+        </form>
+      </main>
+
+      <br />
+      {output ? (
+        <>
+          {output}
+        </>
+      ) : (
+        <>
+        </>
+      )}
+      <br />
+      <footer>
+        <h1>
+          Thank you
+        </h1>
+      </footer>
+    </>
   );
 }
 
